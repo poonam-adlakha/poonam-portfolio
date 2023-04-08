130998f33aca5a95d87d48a3fcc76eaf45324322
Poonam <poonamsoni0391@gmail.com>
2023-04-08 20:29:01 +0530

    Updated mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logos.png'
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-      
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50'>
+      <div>
+        <img src={Logo} alt="logo image" style={{width: '70px'}}/>
+      </div>
+        {/*Menu */}
+        <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+      {/*Hamburger */}
+      <div className='md:hidden z-10'>
+        <FaBars/>
+      </div>
+
+      {/*Mobile Menu */}
+        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
+        </ul>
+
+      {/*Social Icons */}
+      <div className='hidden'>
+
+      </div>
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
