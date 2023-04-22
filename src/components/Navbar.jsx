e49358e336c4d5fdf4177ea1f1f4f36668a56da6
Poonam <poonamsoni0391@gmail.com>
2023-04-22 09:05:03 -0700

    Updated portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import Logo from '../assets/logos.png'
+import React, {useState} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logos.png';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(true)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50'>
       <div>
@@ -17,12 +19,12 @@ const Navbar = () => {
             <li>Contact</li>
         </ul>
       {/*Hamburger */}
-      <div className='md:hidden z-10'>
-        <FaBars/>
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars/> : <FaTimes /> }
       </div>
 
       {/*Mobile Menu */}
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Skills</li>
